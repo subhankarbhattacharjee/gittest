d79e49a318b7b9fcf77c5dba2e16975b38e3b201
Subhankar Bhattacharjee <babai.cset@gmail.com>
2019-05-15 12:18:42 +0530

    Pvt Ltd added

--- App.js
+++ App.js
@@ -21,7 +21,7 @@ export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native! Indus Net Technologies</Text>
+        <Text style={styles.welcome}>Welcome to React Native! Indus Net Technologies Pvt. Ltd.</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
       </View>
